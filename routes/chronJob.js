ad7271204762e548456ff258b3754d3022dedcc1
reapedjuggler <tomarvibhav55@gmail.com>
2021-07-20 15:41:13 +0530

    Changes
    Signed-off-by: reapedjuggler <tomarvibhav55@gmail.com>

--- routes/chronJob.js
+++ routes/chronJob.js
@@ -51,7 +51,10 @@ router.post("/jobs/crondataupdate", async (req, res, next) => {
 
 					console.log(ele._id, "\nIam id in route\n\n");
 
-					desiredPositions = data["desiredPositions"];
+					let desiredPositions = [];
+
+					desiredPositions =
+						data["desiredPositions"] == null ? [] : data["desiredPositions"];
 
 					let desiredJobDetails = [];
 
@@ -191,7 +194,7 @@ router.post("/jobs/crondataupdate", async (req, res, next) => {
 
 router.post("/jobs/cronjob", async (req, res, next) => {
 	// running in every 2 days
-	cron.schedule("*/10 * * * * *", async (req, res, next) => {
+	cron.schedule("0 0 0 */3 * *", async (req, res, next) => {
 		try {
 			var check = await isValid();
 
@@ -272,7 +275,7 @@ router.post("/jobs/stopmatchmaking", async (req, res) => {
 router.post("/jobs/deactivate", async (req, res, next) => {
 	// if anyone on the platform reg before 90 days	then switch activeJobSeeking --> false and mail them if still interested then click and activate again
 
-	cron.schedule("*/10 * * * * *", async (req, res, next) => {
+	cron.schedule("0 0 0 */1 * *", async (req, res, next) => {
 		console.log("Hi from 2 cron\n");
 		const resp = await axios.post(produrl + "/jobs/stopmatchmaking", {});
 
